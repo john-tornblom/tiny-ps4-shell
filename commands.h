f51b40b9dade207156935ace8f531f3e1af6bc6c
John Törnblom <john.tornblom@gmail.com>
2021-01-19 10:48:04 +0100

    add sysctl command

--- commands.h
+++ commands.h
@@ -41,6 +41,7 @@ int main_sfoinfo(int argc, char **argv);
 int main_sfocreate(int argc, char **argv);
 int main_sleep(int argc, char **argv);
 int main_stat(int argc, char **argv);
+int main_sysctl(int argc, char **argv);
 int main_umount(int argc, char **argv);
 int main_uname(int argc, char **argv);
 
